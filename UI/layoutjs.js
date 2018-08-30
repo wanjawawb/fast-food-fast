3deecc3135b815653ab90a29612c657dd2c33cc7
wanjawawb <wanjawawb@gmail.com>
2018-08-30 21:15:06 +0300

    updates of 30-Aug-2018

--- UI/layoutjs.js
+++ UI/layoutjs.js
@@ -2,7 +2,8 @@
 WanjawaWB@gmail.com 
 script to draw banner top of page using HTML5 canvas
 */
-canvas               = O('logo')
+
+canvas               = document.getElementById('banner')
 context              = canvas.getContext('2d')
 context.font         = 'bold italic 80px Georgia'
 context.textBaseline = 'top'
@@ -12,22 +13,10 @@ image.src            = 'pfk.png'
 image.onload = function()
 {
 	gradient = context.createLinearGradient(0, 0, 0, 89)
-	gradient.addColorStop(0.00, '#faa')
-	gradient.addColorStop(0.66, '#f00')
+	gradient.addColorStop(1.00, '#faa') //255,170,170
+	gradient.addColorStop(0.00, '#f00') //255,0,0 - red
 	context.fillStyle = gradient
-	context.fillText(  " Fast Food Fast", 0, 0)
-	context.strokeText("Fast-Food-Fast", 0, 0)
+	context.fillText(  "Fast Food Fast", 0, 0) //hor & ver offsets
+	context.strokeText("Fast-Food-Fast", -10, 0) //hor & ver offsets
 	context.drawImage(image, 64, 32)
-}
-
-function O(i){ 
-	return typeof i == 'object' ? i : document.getElementById(i)
-}
-
-function S(i){ 
-	return O(i).style
-}
-
-function C(i){ 
-	return document.getElementsByClassName(i)
 }
