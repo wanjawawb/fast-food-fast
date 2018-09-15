1ee1d908d62f12bc789fcd419c57094502faa97d
WWB <wanjawawb@gmail.com>
2018-09-15 09:51:44 +0300

    Update server.py

--- API/server.py
+++ API/server.py
@@ -1,30 +1,23 @@
-"""
-Main module of the server file
-"""
-
-# 3rd party moudles
 from flask import render_template
 import connexion
 
 
-# Create the application instance
+# Application instance
 app = connexion.App(__name__, specification_dir='./')
 
-# Read the swagger.yml file to configure the endpoints
+# Read swagger.yml
 app.add_api('swagger.yml')
 
 
-# create a URL route in our application for "/"
+# URL for "/" root
 @app.route('/')
 def home():
     """
-    This function just responds to the browser URL
-    localhost:5000/
+    Rsponds browser URL localhost:5000/
 
-    :return:        the rendered template "home.html"
+    :return:        "home.html"
     """
     return render_template("home.html")
-
 
 if __name__ == '__main__':
     app.run(debug=True)
