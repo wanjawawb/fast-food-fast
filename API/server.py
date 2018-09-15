a93b093c0f7d1732a75481217cc76b8cab6013ce
WWB <wanjawawb@gmail.com>
2018-09-15 09:52:22 +0300

    Update server.py

--- API/server.py
+++ API/server.py
@@ -1,13 +1,11 @@
 from flask import render_template
 import connexion
 
-
 # Application instance
 app = connexion.App(__name__, specification_dir='./')
 
 # Read swagger.yml
 app.add_api('swagger.yml')
-
 
 # URL for "/" root
 @app.route('/')
